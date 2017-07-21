8f81cb30f8b609d3422645134640ab43434f4b47
Evan Barbour <EvanBarbour@hotmail.co.uk>
2017-07-21 01:03:10 +0100

    Not queryable exception

---
+++ app/Models/Exception/ModelNotQueryableException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models\Exception;
+
+/**
+ * Class ModelNotQueryableException
+ *
+ * @author EB
+ * @package App\Models\Exception
+ */
+class ModelNotQueryableException extends \Exception
+{
+
+}
